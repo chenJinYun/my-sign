a1b5c9eb1296999e0e77a04b2f2fafd7e833a2ac
chenjinyun <jinyun.chen@partner.midea.com>
2018-03-06 16:49:59 +0800

    购物车结构调整

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,9 +1,106 @@
   <template>
 	<div id="my_cart">
 		
-		<CartHeaderView></CartHeaderView>
-		<CartMainView></CartMainView>
-		<CartFooterView></CartFooterView>
+        <div id="carttp">
+			<header class="top_bar">
+		    <a onclick="window.history.go(-1)" class="icon_back"></a>
+		    <h3 class="cartname">购物车</h3>
+		    <a href="#" class="icon_menu"></a>
+		</header>
+		</div>
+
+
+		<!-- <CartMainView></CartMainView> -->
+        	<main class="cart_box">
+		    <div class="cart_tip clearfix">
+		        <span>登录后可同步电脑与手机购物车中的商品</span>
+		        <a href="#" class="login">登录</a>
+		    </div>
+		    <div class="cart_content clearfix" v-for="(item,idx) in cartDatas" :key="idx">
+		        <div class="cart_shop clearfix">
+		            <div class="cart_check_box">
+		                <div class="check_box">
+
+		                </div>
+		            </div>
+		            <div class="shop_info clearfix">
+		                <img src="../assets/images/buy-logo.png" alt="" class="shop_icon">
+		                <span class="shop_name">{{item.shop_name}}</span>
+		            </div>
+		            <div class="cart_free clearfix">
+		                <span class="free_tip">优惠券></span>
+		            </div>
+		        </div>
+		        <div class="cart_item">
+		            <div class="cart_item_box">
+		                <div class="check_box">
+
+		                </div>
+		            </div>
+		            <div class="cart_detial_box clearfix">
+		                <a href="#" class="cart_product_link">
+		                    <img v-lazy="item.product_img_url" alt="">
+		                </a>
+		                <div class="item_names">
+		                    <a href="#">
+		                        <span>{{item.product_name}}</span>
+		                    </a>
+		                </div>
+		                <div class="cart_weight">
+		                    <i class="my_weigth">重量:0.45kg</i>
+		                    <span class="my_color">颜色:AT800/16</span>
+		                </div>
+		                <div class="cart_product_sell">
+		                    <span class="product_money">￥<strong class="real_money">{{item.product_uprice}}</strong>.00</span>
+		                    <div class="cart_add clearfix">
+		                        <span class="my_add">+</span>
+		                        <input type="tel" class="my_count" :value="item.goods_num">
+		                        <span class="my_jian">-</span>
+		                    </div>
+		                </div>
+		                <div class="cart_del clearfix">
+		                    <div class="del_top">
+		                    </div>
+		                    <div class="del_bottom">
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		      
+		    </div>
+
+		</main>
+
+        <div class="cart_fo">
+		 <footer class="cart_footer">
+		    <div class="all_check_box">
+		        <div class="check_box">
+
+		        </div>
+		        <span>全选</span>
+		    </div>
+		    <div class="count_money_box">
+		        <div class="heji">
+		            <strong>合计:</strong>
+		            <strong>￥</strong>
+		            <strong>537.80</strong>
+		        </div>
+		        <div class="total_money clearfix">
+		            <span>总额:</span>
+		            <i>￥</i>
+		            <span>537.80</span>
+		            <span>立减:</span>
+		            <i>￥</i>
+		            <span>0.00</span>
+		        </div>>
+		            <a href="#" class="go_pay">
+		                <span>去计算(1)</span>
+		            </a>
+		    </div>
+		 </footer>
+	    </div>
+
+
 		<div class="pop" style="display: none">
 		    <div class="pop_box">
 		        <div class="del_info">
@@ -21,19 +118,16 @@
   </template>
 
 <script>
- import CartHeaderView from './CartHeader.vue';
- import CartMainView from './CartMain.vue';
- import CartFooterView from './CartFooter.vue';
-
  export default {
- 	components:{
- 		CartHeaderView,
- 		CartMainView,
- 		CartFooterView
- 	},
+     data () {
+         return  {
+             cartDatas:[],
+         }
+     },
  	mounted(){
  		this.check();
- 		this.animatDelBox();
+        this.animatDelBox();
+        this.getCartDatas()
      },
      methods: {
        check() {
@@ -84,7 +178,16 @@
                  delUp.style.webkitTransform = "translateY(0px) translateX(0px) rotate(0deg)";
              }
          };
-      }
+      },
+
+      	getCartDatas(){
+  	    	let _this = this;
+  	    	_this.$http.get('/cart').then((res)=>{
+  	    		_this.cartDatas = res.data;
+  	    	},(err)=>{
+  	    		console.log(err);
+  	    	})
+  	    }
     }
  }
 </script>
